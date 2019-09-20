b1321020d390534da602fb76cf62e52d0c8f17b5
Manseerat Kaur <seeratsidhu01@gmail.com>
2019-09-20 19:33:46 -0400

    folder structure

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turducken burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Slammer", FALSE);
+
+INSERT INTO burgers (burger_name. devoured) VALUES ("Veggie Burger", TRUE);

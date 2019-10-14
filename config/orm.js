3bf9a76d1554f1fc714fe391916329f079f601bf
Manseerat Kaur <seeratsidhu01@gmail.com>
2019-10-14 19:05:54 -0400

    added orm methods

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,39 @@
-var connection = require ("../config/connection.js");
+var connection = require("../config/connection.js");
+var table = "burgers";
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+var orm = {
+    selectAll: (table, cb) => {
+        var queryString = `SELECT * FROM ${table};`;
+        connection.query(queryString, (err, result) => {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    insertOne: (table, cols, vals, cb) => {
+        var queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES (${printQuestionMarks(vals.length)});`;
+        connection.query(queryString, vals, (err, result)=> {
+            if(err) throw err;
+            console.log("added");
+            cb(result);
+        });
+    },
+    updateOne: (table, cols, vals, condition, cb) => {
+        var queryString = `UPDATE ${table} SET ${cols.toString()} = ? WHERE ${condition};`;
+        connection.query(queryString, vals, (err, result) => {
+            if(err) throw err;
+            console.log("updated");
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
